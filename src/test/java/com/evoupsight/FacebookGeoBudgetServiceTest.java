74f395b83c488a445af0a6f077f88c36e865d1d0
evoup <yinjia@madhouse-inc.com>
2017-08-06 00:52:08 +0800

    c

--- src/test/java/com/evoupsight/FacebookGeoBudgetServiceTest.java
+++ src/test/java/com/evoupsight/FacebookGeoBudgetServiceTest.java
@@ -1,8 +1,16 @@
 package com.evoupsight;
 
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.apache.commons.math3.stat.descriptive.moment.Mean;
+import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
 import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
 import org.apache.commons.math3.stat.regression.SimpleRegression;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 /**
  * Created by evoup on 2017/8/3.
@@ -58,8 +66,8 @@ public class FacebookGeoBudgetServiceTest {
         double rSquared = regression.calculateRSquared();
 
         double sigma = regression.estimateRegressionStandardError();
-        System.out.println("prediction for 1.5 = " + predict(new double[] {1.5, 1, 1, 0, 0.3}, beta));
-;    }
+        System.out.println("prediction for 1.5 = " + predict(new double[]{1.5, 1, 1, 0, 0.3}, beta));
+    }
 
     /**
      * test regression3
@@ -95,9 +103,8 @@ public class FacebookGeoBudgetServiceTest {
         double rSquared = regression.calculateRSquared();
 
         double sigma = regression.estimateRegressionStandardError();
-        System.out.println("prediction for 1.5 = " + predict(new double[] {1.5}, beta));
-        ;    }
-
+        System.out.println("prediction for 1.5 = " + predict(new double[]{1.5}, beta));
+    }
 
 
     private double predict(double[] data, double[] beta) {
@@ -107,4 +114,90 @@ public class FacebookGeoBudgetServiceTest {
         }
         return result;
     }
+
+    @Test
+    public void t0() {
+        DescriptiveStatistics stats = new DescriptiveStatistics();
+        // Add the data from the array
+        List<Double> lines = Arrays.asList(1.2, 2.4, 5.1, 4.3, 4.1,3.1,222.1);
+        for( int i = 0; i < lines.size(); i++) {
+            stats.addValue(lines.get(i));
+        }
+        double mean = stats.getMean();
+        double sd = stats.getStandardDeviation();
+        double median = stats.getPercentile(50);
+//        Mean mean = new Mean();
+//        mean.evaluate(lines.stream().mapToDouble(d -> d).toArray());
+//        StandardDeviation sd = new StandardDeviation();
+//        sd.evaluate(lines.stream().mapToDouble(d -> d).toArray());
+        double threshold = 1.2;
+        List<Double> result = lines.stream()                // convert list to stream
+                .filter(line -> test(line, mean, sd, threshold))     // we dont like mkyong
+                .collect(Collectors.toList());              // collect the output and convert streams to a List
+
+        result.forEach(System.out::println);                //output : spring, node
+    }
+
+    public boolean test(double sampleValue, double mean, double sd, double threshold) {
+        double multpleSd = threshold * sd;
+        double absMean = Math.abs(mean);
+        return sampleValue > absMean + multpleSd;
+    }
+
+//    @Test
+//    private void t() throws Exception {
+//        final double threshold = 2.0;
+//
+//        Topology t = new Topology("StandardDeviationFilter");
+//
+//        final Random rand = new Random();
+//
+//        // Produce a stream of random double values with a normal
+//        // distribution, mean 0.0 and standard deviation 1.
+//        TStream<Double> values = t.limitedSource(new Supplier<Double>() {
+//            public Double get() {
+//                return rand.nextGaussian();
+//            }
+//
+//            private static final long serialVersionUID = 1L;
+//
+//
+//
+//        }, 100000);
+//
+//        /*
+//         * Filters the values based on calculating the mean and standard
+//         * deviation from the incoming data. In this case only outliers are
+//         * present in the output stream outliers. A outlier is defined as one
+//         * more than (threshold*standard deviation) from the mean.
+//         *
+//         * This demonstrates an anonymous functional logic class that is
+//         * stateful. The two fields mean and sd maintain their values across
+//         * multiple invocations of the test method, that is for multiple tuples.
+//         *
+//         * Note both Mean & StandardDeviation classes are serializable.
+//         */
+//        TStream<Double> outliers = values.filter(new Predicate<Double>() {
+//
+//            private static final long serialVersionUID = 1L;
+//            private final Mean mean = new Mean();
+//            private final StandardDeviation sd = new StandardDeviation();
+//
+//
+//            public boolean test(Double tuple) {
+//                mean.increment(tuple);
+//                sd.increment(tuple);
+//
+//                double multpleSd = threshold * sd.getResult();
+//                double absMean = Math.abs(mean.getResult());
+//                double absTuple = Math.abs(tuple);
+//
+//                return absTuple > absMean + multpleSd;
+//            }
+//        });
+//
+//        outliers.print();
+//
+//        StreamsContextFactory.getEmbedded().submit(t).get();
+//    }
 }
